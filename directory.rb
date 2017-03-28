fd36d39279943a896eaab60bdccb7be6e79416d2
Ben Taylor <taylor.ben89@gmail.com>
2017-03-28 12:11:33 +0000

    Added comments

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# Print each student name on different line
 puts "The students of Villains Academy"
 puts "-------------"
 puts "Dr. Hannibal Lecter"
@@ -11,6 +12,8 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+# print number of students on one line
 print "Overall we have "
+# use print instead of puts
 print 11
 print " great students"
