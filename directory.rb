b53daffcab58f9982b49061fea5a8dca4186839b
Ben Taylor <taylor.ben89@gmail.com>
2017-04-03 10:37:20 +0000

    interactive menu

--- directory.rb
+++ directory.rb
@@ -52,8 +52,27 @@ def input_students
     students
 end
 
+def interactive_menu
+    students = []
+    loop do
+      puts "1. Input the students."
+      puts "2. Show the students."
+      puts "9. Exit"
+    selection = gets.chomp
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit # terminates the program
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+end
+
+interactive_menu
